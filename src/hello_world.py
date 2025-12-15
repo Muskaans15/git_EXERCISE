c6821205ad5d6ccda180523f2d6f6472caa4c3ac
Muskaan Singh <127521321+Muskaans15@users.noreply.github.com>
2025-12-15 11:01:43 +0530

    Update hello_world.py

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,6 +1,8 @@
 class HelloWorld:
     def main(self):
         print("Hello World – edited directly on GitHub")
+        print("This line was added on GitHub")
+
 
 if __name__ == "__main__":
     HelloWorld().main()
